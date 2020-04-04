a46d8e127c074d79ae16d37831458ff2e02c807e
vlad <vladov3000@gmail.com>
2020-04-03 22:56:13 -0700

    npm packages and server.js set up

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-var express = require('express')
-  , app = express()
+
+// load dependencies
+var express = require('express'),
+  logger = require('morgan'),
+  app = express()
 
 app.use(logger('dev'))
 app.use(express.static(__dirname + '/static'))
