51bb59910139f65fd5fae69952aaef39f66d528f
Andrew Wu <drewgu9@gmail.com>
2020-04-09 17:27:38 -0700

    updated twitter

--- twitter/twitter_workflow.py
+++ twitter/twitter_workflow.py
@@ -1,9 +1,10 @@
 import twitter
 
-CONSUMER_KEY = "dQXFolLAYrgAkeHeur2avDzbc"
-CONSUMER_SECRET = "YK7FN6SNehS9MXfS2tWlrTYSOGt6aI3gUJdqceQzFYq3r6K16q"
-ACCESS_TOKEN_KEY = "949893216928645121-UowtQ9XSP5Mn0Ai0RbRBpRNDTAq4aZO"
-ACCESS_TOKEN_SECRET = "fU2VZsxJX651cbXoRSEn5e4MJVbYRdhG9S0SYISoJj2Vh"
+#User specifies their own API key
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+ACCESS_TOKEN_KEY = ""
+ACCESS_TOKEN_SECRET = ""
 
 def main():
 
@@ -19,12 +20,39 @@ def main():
     twitter_ID = input("What is the Twitter ID of your specified user? ")
     twitter_name = input("What is the Twitter Name of the specified user? ")
 
-    users_friends = api.GetFriendIDsPaged(user_id=twitter_ID, screen_name = twitter_name)
-    print("List of " + twitter_name + " friend's IDs: ")
+    users_friends_ID = api.GetFriends(user_id=twitter_ID, screen_name = twitter_name)
 
-    print(users_friends)
+    friends = []
 
+    for friend in users_friends_ID:
+        friends.append(friend.name)
     
+    recent_status = api.GetUserTimeline(count=10, screen_name=twitter_name)
+
+    tweets = []
     
+    for tweet in recent_status:
+        tweets.append(tweet.full_text)
+
+    #Writes user name, friends list and status list to file
+    store_in_file(tweets, friends, twitter_name)
+
+
+def store_in_file(tweets, friends, twitter_name):
+    with open("twitteroutput.txt","w+") as f:
+        f.write("User Name: " + twitter_name + "\n")
+        f.write("User Friends: " )
+    
+        for n in friends:
+            f.write(n + ", ")
+        
+        f.write("\n")
+    
+        f.write("User Status: " + "\n")
+
+        for t in tweets:
+            f.write(t + ", ")
+        f.close()
+
 if __name__ == "__main__":
     main()
